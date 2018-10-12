d9f39fe4267b5c5521b80a59c4d7f802c7d9dead
Thanh Huynh <hu.thanh89@yahoo.com>
2018-10-12 13:17:27 -0700

    Fix menu link click

--- src/js/component/menu/layout.js
+++ src/js/component/menu/layout.js
@@ -25,17 +25,20 @@ class Layout extends React.Component {
         </div>
 
         <div className="row">
-          <div className="col-12">
+          <div className="col-md-5 m-auto">
             <Link to='/game/add'>
-              <button className="btn btn-outline-success btn-lg menu-button">
+              <button className="btn btn-outline-success btn-lg menu-button btn-block">
                 <span>Add</span>
                 <i className="fas fa-plus fa-lg"></i>
               </button>
             </Link>
           </div>
-          <div className="col-12">
+        </div>
+
+        <div className="row">  
+          <div className="col-md-5 m-auto">
             <Link to='/game/subtract'>
-              <button className="btn btn-outline-info btn-lg menu-button">
+              <button className="btn btn-outline-info btn-lg menu-button btn-block">
                 <span>Subtract</span>
                 <i className="fas fa-minus fa-lg"></i>
               </button>
@@ -44,17 +47,20 @@ class Layout extends React.Component {
         </div>
         
         <div className="row">
-          <div className="col-12">
+          <div className="col-md-5 m-auto">
             <Link to='/game/multiply'>
-              <button className="btn btn-outline-warning btn-lg menu-button">
+              <button className="btn btn-outline-warning btn-lg menu-button btn-block">
                 <span>Multiply</span>
                 <i className="fas fa-times fa-lg"></i>
               </button>            
             </Link>
           </div>
-          <div className="col-12">
+        </div>
+
+        <div className="row">  
+          <div className="col-md-5 m-auto">
             <Link to='/game/divide'>
-              <button className="btn btn-outline-danger btn-lg menu-button">
+              <button className="btn btn-outline-danger btn-lg menu-button btn-block">
                 <span>Divide</span>
                 <i className="fas fa-divide fa-lg"></i>
               </button>
