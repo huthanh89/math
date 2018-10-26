62ff66be9819f823416bbbefa629dcb40775e37c
Thanh Huynh <hu.thanh89@yahoo.com>
2018-10-25 19:19:03 -0700

    Reduce min max

--- src/js/reducer/game/level.js
+++ src/js/reducer/game/level.js
@@ -27,12 +27,33 @@ const levelModel = () => {
 };
 
 //-----------------------------------------------------------------------------//
+// Add
+//-----------------------------------------------------------------------------//
 
 function add(state, operator) {
 
   let operandA = 0;
   let operandB = 0;
   let i =        0;
+  let min =      0;
+  let max =      0;
+
+  switch(state.difficulty){
+    case 1: {
+      min = 50;
+      max = 100;
+      break;
+    }
+    case 2: {
+      min = 100;
+      max = 1000;
+      break;
+    }
+    default:{
+      min = 1;
+      max = 50;
+    }
+  }
 
   for(i; i<10; i++){
 
@@ -41,8 +62,8 @@ function add(state, operator) {
     // for the inputs.
 
     do{
-      operandA = chance.integer({ min: 1, max: 99 });
-      operandB = chance.integer({ min: 1, max: 99 });
+      operandA = chance.integer({ min: min, max: max });
+      operandB = chance.integer({ min: min, max: max });
     }
     while(Math.abs(operandA - operandB) < 5);
 
@@ -60,17 +81,40 @@ function add(state, operator) {
   return state;
 }
 
+//-----------------------------------------------------------------------------//
+// Subtract
+//-----------------------------------------------------------------------------//
+
 function subtract(state, operator) {
 
   let operandA = 0;
   let operandB = 0;
-  let i        = 0;
+  let i =        0;
+  let min =      0;
+  let max =      0;
+
+  switch(state.difficulty){
+    case 1: {
+      min = 50;
+      max = 100;
+      break;
+    }
+    case 2: {
+      min = 100;
+      max = 1000;
+      break;
+    }
+    default:{
+      min = 1;
+      max = 30;
+    }
+  }
 
   for(i; i<10; i++){
 
     do{
-      operandA = chance.integer({ min: 1, max: 99 });
-      operandB = chance.integer({ min: 1, max: 99 });
+      operandA = chance.integer({ min: min, max: max });
+      operandB = chance.integer({ min: min, max: max });
 
       if(operandA < operandB){
         let temp = operandA;
@@ -95,17 +139,40 @@ function subtract(state, operator) {
   return state;
 }
 
+//-----------------------------------------------------------------------------//
+// Multiply
+//-----------------------------------------------------------------------------//
+
 function multiply(state, operator) {
 
   let operandA = 0;
   let operandB = 0;
-  let i        = 0;
+  let i =        0;
+  let min =      0;
+  let max =      0;
+
+  switch(state.difficulty){
+    case 1: {
+      min = 1;
+      max = 30;
+      break;
+    }
+    case 2: {
+      min = 10;
+      max = 500;
+      break;
+    }
+    default:{
+      min = 1;
+      max = 12;
+    }
+  }
 
   for(i; i<10; i++){
 
     do{
-      operandA = chance.integer({ min: 1, max: 20 });
-      operandB = chance.integer({ min: 1, max: 20 });
+      operandA = chance.integer({ min: min, max: max });
+      operandB = chance.integer({ min: min, max: max });
     }
     while(Math.abs(operandA - operandB) < 5);
 
@@ -123,17 +190,40 @@ function multiply(state, operator) {
   return state;
 }
 
+//-----------------------------------------------------------------------------//
+// Divide
+//-----------------------------------------------------------------------------//
+
 function divide(state, operator) {
   let operandA = 0;
   let operandB = 0;
   let answer   = 0;
   let i        = 0;
+  let min      = 0;
+  let max      = 0;
+
+  switch(state.difficulty){
+    case 1: {
+      min = 5;
+      max = 25;
+      break;
+    }
+    case 2: {
+      min = 10;
+      max = 100;
+      break;
+    }
+    default:{
+      min = 1;
+      max = 10;
+    }
+  }
 
   for(i; i<10; i++){
 
     do{
-      answer   = chance.integer({ min: 1, max: 10 });
-      operandA = chance.integer({ min: 1, max: 10 });
+      answer   = chance.integer({ min: min, max: max });
+      operandA = chance.integer({ min: min, max: max });
       operandB = answer * operandA;
     }
     while(answer < 5);
@@ -151,6 +241,10 @@ function divide(state, operator) {
 
   return state;
 }
+
+//-----------------------------------------------------------------------------//
+// Switch
+//-----------------------------------------------------------------------------//
 
 function populate(state, operator) {
   switch(operator){
