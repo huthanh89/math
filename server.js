d98665feef57e3961c3256fd0bc6124c9e344f35
Thanh Huynh <hu.thanh89@yahoo.com>
2018-10-29 19:44:51 -0700

    database wip

--- server.js
+++ server.js
@@ -6,6 +6,46 @@ const path        = require('path');
 const express     = require('express');
 const compression = require('compression')
 const app         = express();
+/*
+const mongoose    = require('mongoose');
+
+//-----------------------------------------------------------------------------//
+// Schema
+//-----------------------------------------------------------------------------//
+
+const User = require('./server/model/user.js');
+
+//-----------------------------------------------------------------------------//
+// Connect to Mongo DB
+//-----------------------------------------------------------------------------//
+
+mongoose.connect('mongodb://localhost/test', {
+    useNewUrlParser: true
+});
+
+mongoose.Promise = global.Promise;
+
+//-----------------------------------------------------------------------------//
+// Create a document
+
+const user = new User.Model({
+    name: 'goo',
+    coin: 88
+});
+
+user.save(function (err) {
+    if (err) {
+        console.log(err);
+    } 
+    else {
+        console.log('meow');
+    }
+});
+*/
+
+//-----------------------------------------------------------------------------//
+// Configure App
+//-----------------------------------------------------------------------------//
 
 // Tell express to look for views in the following directory.
 
@@ -19,12 +59,10 @@ app.set('view engine', 'html');
 
 app.use(compression());
 
-/*
 app.use('/', function(req, res, next){
     console.log('MathTingz', req.url);
     next()
 });
-*/
 
 // Tell how to serve files and where to look.
 
@@ -35,7 +73,9 @@ app.use('/game', express.static(__dirname + '/dist'));
 app.use('/game/:type', express.static(__dirname + '/dist'));
 app.use('/summary', express.static(__dirname + '/dist'));
 
+//-----------------------------------------------------------------------------//
 // Handle route.
+//-----------------------------------------------------------------------------//
 
 app.get('/', function(req, res){
     res.render('index.html');
@@ -57,12 +97,17 @@ app.get('/summary', function(req, res){
     res.render('index.html');
 });
 
+/*
 app.get('/*', function(req, res){
     res.redirect("/mathtingz/");
 });
+*/
 
-
+//-----------------------------------------------------------------------------//
+// Start Application
+//
 // Listen app on the following port.
+//-----------------------------------------------------------------------------//
 
 app.listen(3002, () => console.log('Math App listening on port 3002'))
 
