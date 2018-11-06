a4ecd816fb416b16dbbe2f8c8a814de087c4149b
Thanh Huynh <hu.thanh89@yahoo.com>
2018-11-06 15:10:16 -0800

    Add new monsters

--- src/js/lib/creature.js
+++ src/js/lib/creature.js
@@ -6,71 +6,175 @@ var Creatures = [
         levelExp: 10,
         feed:     73,
         bonus:    1.5,
-        name:    'Shrimp',
-        src:     'avatar_shrimp.png'
+        src:     'monster1.png'
     },
     {
         id:       1,
+        price:    0,
+        levelExp: 10,
+        feed:     73,
+        bonus:    1.5,
+        src:     'monster2.png'
+    },
+    {
+        id:       2,
+        price:    0,
+        levelExp: 10,
+        feed:     73,
+        bonus:    1.5,
+        src:     'monster3.png'
+    },
+    {
+        id:       3,
         price:    100,
         levelExp: 100,
         feed:     220,
         bonus:    14,
-        name:    'Tuna',
-        src:     'avatar_tuna.png'
+        src:     'monster4.png'
     },
     {
-        id:       2,
+        id:       4,
+        price:    200,
+        levelExp: 200,
+        feed:     320,
+        bonus:    24,
+        src:     'monster5.png'
+    },
+    {
+        id:       5,
+        price:    400,
+        levelExp: 400,
+        feed:     620,
+        bonus:    30,
+        src:     'monster6.png'
+    },
+    {
+        id:       6,
+        price:    600,
+        levelExp: 600,
+        feed:     820,
+        bonus:    35,
+        src:     'monster7.png'
+    },
+    {
+        id:       7,
+        price:    800,
+        levelExp: 900,
+        feed:     1000,
+        bonus:    40,
+        src:     'monster8.png'
+    },
+    {
+        id:       8,
+        price:    1000,
+        levelExp: 1100,
+        feed:     1300,
+        bonus:    45,
+        src:     'monster9.png'
+    },
+    {
+        id:       9,
         price:    1500,
         levelExp: 1000,
         feed:     1740,
-        bonus:    42,
-        name:    'Octopus',
-        src:     'avatar_octopus.png'
+        bonus:    55,
+        src:     'monster10.png'
     },
     {
-        id:       3,
+        id:       10,
+        price:    2500,
+        levelExp: 3000,
+        feed:     3740,
+        bonus:    60,
+        src:     'monster11.png'
+    },
+    {
+        id:       11,
         price:    4250,
         levelExp: 1400,
         feed:     3450,
         bonus:    65,
-        name:    'Turtle',
-        src:     'avatar_turtle.png'
+        src:     'monster12.png'
     },
     {
-        id:       4,
+        id:       12,
+        price:    7000,
+        levelExp: 7000,
+        feed:     9740,
+        bonus:    80,
+        src:     'monster13.png'
+    },
+    {
+        id:       13,
         price:    8000,
-        levelExp: 13000,
-        feed:     25760,
-        bonus:    163,
-        name:    'Crab',
-        src:     'avatar_crab.png'
+        levelExp: 9000,
+        feed:     12760,
+        bonus:    150,
+        src:     'monster14.png'
     },
     {
-        id:       5,
+        id:       14,
+        price:    10000,
+        levelExp: 100,
+        feed:     15740,
+        bonus:    200,
+        src:     'monster15.png'
+    },
+    {
+        id:       15,
         price:    12000,
         levelExp: 16400,
         feed:     21230,
         bonus:    515,
-        name:    'Shark',
-        src:     'avatar_shark.png'
+        src:     'monster16.png'
     },
     {
-        id:       6,
+        id:       16,
         price:    32000,
         levelExp: 11600,
         feed:     21650,
         bonus:    1818,
-        name:    'Killer Whale',
-        src:     'avatar_killer_whale.png'
+        src:     'monster17.png'
     },
     {
-        id:       7,
+        id:       17,
+        price:    45000,
+        levelExp: 45000,
+        feed:     71500,
+        bonus:    2400,
+        src:     'monster18.png'
+    },
+    {
+        id:       18,
         price:    64000,
         levelExp: 31000,
         feed:     41500,
         bonus:    3400,
-        name:    'Whale',
-        src:     'avatar_whale.png'
+        src:     'monster19.png'
+    },
+    {
+        id:       19,
+        price:    84000,
+        levelExp: 61000,
+        feed:     71500,
+        bonus:    4000,
+        src:     'monster20.png'
+    },
+    {
+        id:       20,
+        price:    100000,
+        levelExp: 91000,
+        feed:     101500,
+        bonus:    4400,
+        src:     'monster21.png'
+    },
+    {
+        id:       21,
+        price:    225000,
+        levelExp: 125000,
+        feed:     174000,
+        bonus:    5000,
+        src:     'monster22.png'
     }
 ]; 
 
